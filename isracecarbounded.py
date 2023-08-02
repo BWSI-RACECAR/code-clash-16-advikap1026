835896c4833e2a00597edddde16760e25736ab31
advikap1026 <127911640+advikap1026@users.noreply.github.com>
2023-08-02 09:20:30 -0400

    Add files via upload

--- isracecarbounded.py
+++ isracecarbounded.py
@@ -8,23 +8,48 @@ class Solution:
             cur_dir=2
             
             #TODO: Write code below to returnn a boolean value with the solution to the prompt.
-            previousChar = previousChar = instructions[0]
-            answer = False
-            print("instructions: " + instructions)
-            for i in range(len(instructions)):
-                 if i == 0: 
-                      continue
+            for x in range(4):
+                 for i in instructions: 
+                        if i == "G":
+                            if cur_dir == 2:
+                                cur_pos[1] = cur_pos[1] + 1
+                            if cur_dir == 0: 
+                                cur_pos[1] = cur_pos[1]-1
+                            if cur_dir == 3:
+                                 cur_pos[0] = cur_pos[0]+1
+                            if cur_dir ==1:
+                                 cur_pos[0] = cur_pos[0]-1
+                        if i == "L":
+                             if cur_dir == 0: 
+                                  cur_dir =3
+                             else: 
+                                  cur_dir = cur_dir -1
+                        if i == "R":
+                             if cur_dir ==0: 
+                                  cur_dir = 0
+                             else: 
+                                  cur_dir = cur_dir + 1
+            if cur_pos == initial_pos:
+                 return True 
+            else: 
+                 return False 
+            # previousChar = previousChar = instructions[0]
+            # answer = False
+            # print("instructions: " + instructions)
+            # for i in range(len(instructions)):
+            #      if i == 0: 
+            #           continue
                       
-                 else:
+            #      else:
                       
-                      if ((previousChar == instructions[i] and instructions[i] == "L") or (previousChar == instructions[i] and instructions[i] == "R")):
+            #           if ((previousChar == instructions[i] and instructions[i] == "L") or (previousChar == instructions[i] and instructions[i] == "R")):
                            
-                           answer = True
-                           previousChar = instructions[i]
-                           print("prev char: " + previousChar)
-                           break 
+            #                answer = True
+            #                previousChar = instructions[i]
+            #                print("prev char: " + previousChar)
+            #                break 
             
-            return answer
+            # return answer
                       
             pass
         
